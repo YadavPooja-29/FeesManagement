ca1cd0b2336b822670f0f5457e0388f14dca2f73
Pooja Yadav <100533707+YadavPooja-29@users.noreply.github.com>
2022-03-06 02:46:08 +0530

    Add files via upload

---
+++ src/com/main/dao/Student.java
@@ -0,0 +1,85 @@
+package com.main.dao;
+
+public class Student {
+
+	public String name;
+	public String email;
+	public String cource;
+	public String fee;
+	public String paid;
+	public String due;
+	public String address;
+	public String city;
+	public String state;
+	public String country;
+	public String contact;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCource() {
+		return cource;
+	}
+	public void setCource(String cource) {
+		this.cource = cource;
+	}
+	public String getFee() {
+		return fee;
+	}
+	public void setFee(String fee) {
+		this.fee = fee;
+	}
+	public String getPaid() {
+		return paid;
+	}
+	public void setPaid(String paid) {
+		this.paid = paid;
+	}
+	public String getDue() {
+		return due;
+	}
+	public void setDue(String due) {
+		this.due = due;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getContact() {
+		return contact;
+	}
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+	
+	
+	
+}
---
+++ src/com/main/dao/User.java
@@ -0,0 +1,42 @@
+package com.main.dao;
+
+public class User {
+	public String name;
+	public String password;
+	public String email;
+	public String contact;
+	public String type;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getContact() {
+		return contact;
+	}
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	
+}
